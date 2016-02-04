fb00d975c429fb7322ec4a029483c64f34836ee5
Frank Liu <ke.frank.liu@gmail.com>
2016-02-04 14:14:50 +1100

    added input element

--- LearnJS/LearnJS.Web/app/app.component.ts
+++ LearnJS/LearnJS.Web/app/app.component.ts
@@ -1,7 +1,22 @@
 ﻿import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
+//Component decorator make a class as an Angular component
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>{{title}}</h1>
+<h2>{{hero.name}}</h2>
+<div><label>id: </label>{{hero.id}}</div>
+<div><label>name: </label>
+<div><input value="{{hero.name}}" placeholder="name"></div>
+</div>`
 })
-export class AppComponent { }
+
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero = {id: 1, name: 'LiuKe'};
+}
