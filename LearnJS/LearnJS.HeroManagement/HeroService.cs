25c71dacf3ca6bcef7f4d62dc48063e59ba7bf0e
Frank Liu <ke.frank.liu@gmail.com>
2016-02-11 14:45:27 +1100

    added Web API project and HeroManagement Bounded Context

---
+++ LearnJS/LearnJS.HeroManagement/HeroService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnJS.HeroManagement
+{
+    public class HeroService
+    {
+        private readonly string _nameOrConnectionString;
+
+        public HeroService(string nameOrConnectionString="HeroManagement")
+        {
+            this._nameOrConnectionString = nameOrConnectionString;
+        }
+
+        public Guid CreateHero
+    }
+}
