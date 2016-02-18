663dc02f5bd78ad579cba12f8593fa649948a5db
Frank Liu <ke.frank.liu@gmail.com>
2016-02-18 22:43:44 +1100

    added myHighlight attribute directive

---
+++ LearnJS/LearnJS.Web/app/attribute-directives/highlight.directive.ts
@@ -0,0 +1,10 @@
+﻿import {Directive, ElementRef, Input} from 'angular2/core' 
+
+@Directive({
+    selector: '[myHighlight]'
+})
+export class HightlightDirective{
+    constructor(el: ElementRef) {
+        el.nativeElement.style.backgroundColor = 'yellow';
+    }
+}
